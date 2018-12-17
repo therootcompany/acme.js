642a25935e3a954e3d54205b1097c94446726ba2
AJ ONeal <coolaj86@gmail.com>
2018-12-16 21:19:20 -0700

    v1.3.1: reduce deps, update rsa-compat, fix rando JWK bug

--- node.js
+++ node.js
@@ -1,8 +1,7 @@
-/*!
- * acme-v2.js
- * Copyright(c) 2018 AJ ONeal <aj@ppl.family> https://ppl.family
- * Apache-2.0 OR MIT (and hence also MPL 2.0)
- */
+// Copyright 2018 AJ ONeal. All rights reserved
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 'use strict';
 /* globals Promise */
 
