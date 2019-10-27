0b91d9a26def6f994c5e25563c333e12df1a4daa
AJ ONeal <coolaj86@gmail.com>
2019-10-27 00:55:40 -0600

    add https sni example

---
+++ examples/https-sni-server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var https = require('http2');
+var tls = require('tls');
+var fs = require('fs');
+
+var key = fs.readFileSync('./privkey.pem');
+var cert = fs.readFileSync('./fullchain.pem');
+
+function SNICallback(servername, cb) {
+	console.log('sni:', servername);
+	cb(null, tls.createSecureContext({ key, cert }));
+}
+
+var server = https
+	.createSecureServer({ SNICallback: SNICallback }, function(req, res) {
+		res.end('Hello, Encrypted World!');
+	})
+	.listen(443, function() {
+		console.info('Listening on', server.address());
+	});
